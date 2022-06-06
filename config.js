c2359ecbb735e099e4f70cdee0cbdd553a56fa0b
nvthong2303 <nvthong2303@gmail.com>
2022-06-07 00:52:52 +0700

    pushcode

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Hi !',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao cậu cứ đi nhầm đường
+    lạc vào tim tớ vậy`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng cậu đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích tớ <33',
+    btnNo: 'Không, anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h tớ qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
