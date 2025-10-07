df0bc79acd4fd5aec08e540a9ad4c323e770580c
nvthong <nvthong2303@gmail.com>
2025-10-07 20:33:42 +0700

    Update config and HTML title for personalization; replace 'cậu' with 'em' and adjust related text. Also, update background image.

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     introTitle: 'Cin chào',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Xuân cứ đi nhầm đường
+    Mà sao em cứ đi nhầm đường
     lạc vào tim tớ vậy`,
     btnIntro: 'hihi',
-    title: 'Phải chăng cậu đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích tớ <33',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, anh trai à :3',
-    question:'Hôm nào đi chơi với tớ nhé 😙',
-    btnReply: 'Gửi cho tớ <3',
+    question:'Hôm nào đi chơi với anh nhé 😙',
+    btnReply: 'Gửi cho anh <3',
     reply: 'okeeee hôm nào mình đi chơi đi <33333333',
-    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Cuối tuần tớ qua đón nhé công chúa.',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Cuối tuần anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/nvthong2303' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
